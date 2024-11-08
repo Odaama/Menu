579f189e402eef61efcefc389e94add2eb6abe72
Christian Ek <Christian.ek@live.com>
2024-11-08 13:09:26 +0100

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Christian Ek on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MenuTests/MenuTests.swift
@@ -0,0 +1,16 @@
+//
+//  MenuTests.swift
+//  MenuTests
+//
+//  Created by Christian Ek on 2024-11-08.
+//
+
+import Testing
+
+struct MenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
